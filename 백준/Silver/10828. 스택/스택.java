faf6b28f7f9e0ae622af83742319e7554ad81b3e
Dongick <87112589+Dongick@users.noreply.github.com>
2024-06-24 15:28:41 +0900

    [Silver IV] Title: 스택, Time: 184 ms, Memory: 19944 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -7,40 +7,37 @@ class Main{
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
+        StringTokenizer st;
         while(T-- > 0) {
-            String[] s = br.readLine().split(" ");
-            if(s[0].equals("push")) {
-                stack.push(Integer.parseInt(s[1]));
-            } else {
-                switch(s[0]) {
-                    case "top":
-                        if(!stack.isEmpty()) {
-                            sb.append(stack.peek()).append("\n");
-                            break;
-                        } else {
-                            sb.append(-1).append("\n");
-                            break;
-                        }
-                    case "size":
-                        sb.append(stack.size()).append("\n");
-                        break;
-                    case "empty":
-                        if(stack.isEmpty()) {
-                            sb.append(1).append("\n");
-                            break;
-                        } else {
-                            sb.append(0).append("\n");
-                            break;
-                        }
-                    case "pop":
-                        if(stack.isEmpty()) {
-                            sb.append(-1).append("\n");
-                            break;
-                        } else {
-                            sb.append(stack.pop()).append("\n");
-                            break;
-                        }
-                }
+            st = new StringTokenizer(br.readLine());
+            switch(st.nextToken()) {
+                case "push":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "top":
+                    if(!stack.isEmpty()) {
+                        sb.append(stack.peek()).append("\n");
+                    } else {
+                        sb.append(-1).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(stack.size()).append("\n");
+                    break;
+                case "empty":
+                    if(stack.isEmpty()) {
+                        sb.append(1).append("\n");
+                    } else {
+                        sb.append(0).append("\n");
+                    }
+                    break;
+                case "pop":
+                    if(stack.isEmpty()) {
+                        sb.append(-1).append("\n");
+                    } else {
+                        sb.append(stack.pop()).append("\n");
+                    }
+                    break;
             }
         }
         System.out.println(sb);
