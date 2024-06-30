d07e62ef94c27a55d9c7bfc6a1fee1074eb88ca3
Dongick <87112589+Dongick@users.noreply.github.com>
2024-06-30 14:10:25 +0900

    [Silver II] Title: 베르트랑 공준, Time: 164 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    static boolean[] arr;
+    static int maxNum = 123456 * 2 + 1;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        arr = new boolean[maxNum];
+        arr[1] = true;
+        function();
+        while(true) {
+            int n = Integer.parseInt(br.readLine());
+            if(n == 0)
+                break;
+            int count = 0;
+            for(int i = n+1; i <= n*2; i++) {
+                if(!arr[i])
+                    count++;
+            }
+            sb.append(count).append("\n");
+        }
+        System.out.println(sb);
+    }
+    
+    static void function() {
+        for(int i = 2; i < maxNum; i++) {
+            for(int j = i*2; j < maxNum; j += i) {
+                if(!arr[j])
+                    arr[j] = true;
+            }
+        }
+    }
+}
