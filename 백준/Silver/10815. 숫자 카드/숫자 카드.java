9274934a187be7b1d562c1a800d2984aafa116f1
Dongick <87112589+Dongick@users.noreply.github.com>
2024-07-11 15:06:09 +0900

    [Silver V] Title: 숫자 카드, Time: 752 ms, Memory: 119124 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -3,39 +3,34 @@ import java.io.*;
 import java.util.stream.Collectors;
 
 class Main {
-    static int[] arr;
-    static int n;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n];
+        int n = Integer.parseInt(br.readLine());
+        boolean[] arr = new boolean[20000001];
         StringTokenizer st= new StringTokenizer(br.readLine());
         for(int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            if(num >= 0)
+                arr[10000000 + num] = true;
+            else
+                arr[Math.abs(num)] = true;
         }
-        Arrays.sort(arr);
         int m = Integer.parseInt(br.readLine());
         st= new StringTokenizer(br.readLine());
         for(int i = 0; i < m; i++) {
             int num = Integer.parseInt(st.nextToken());
-            sb.append(BinarySearch(num)).append(" ");
+            if(num >= 0) {
+                if(arr[10000000 + num])
+                    sb.append(1).append(" ");
+                else
+                    sb.append(0).append(" ");
+            } else
+                if(arr[Math.abs(num)])
+                    sb.append(1).append(" ");
+                else
+                    sb.append(0).append(" ");
         }
         System.out.println(sb);
-    }
-
-    static int BinarySearch(int num) {
-        int start = 0;
-        int end = n-1;
-        while(start <= end) {
-            int mid = (start + end) / 2;
-            if(arr[mid] == num)
-                return 1;
-            else if(arr[mid] > num)
-                end = mid - 1;
-            else
-                start = mid + 1;
-        }
-        return 0;
     }
 }
