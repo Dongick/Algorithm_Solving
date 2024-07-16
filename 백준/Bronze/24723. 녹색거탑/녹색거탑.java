ce3b5796afa48b412a92cb388d90a268893186ec
Dongick <87112589+Dongick@users.noreply.github.com>
2024-07-16 17:44:10 +0900

    [Bronze IV] Title: 녹색거탑, Time: 108 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        sb.append((int)Math.pow(2,n));
+        System.out.println(sb);
+    }
+}
