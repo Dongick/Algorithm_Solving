532c7c77a1886eefd20edeffcb0bbcf8bbaeeb45
Dongick <87112589+Dongick@users.noreply.github.com>
2024-07-23 16:53:44 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 840 ms, Memory: 105468 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -6,20 +6,23 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        String s = br.readLine();
+        char[] c = br.readLine().toCharArray();
+        int[][] arr = new int[26][c.length + 1];
+        for(int i = 0; i < 26; i++) {
+            for(int j = 0; j < c.length; j++) {
+                if(c[j] - 'a' == i) {
+                    arr[i][j+1] = arr[i][j] + 1;
+                } else
+                    arr[i][j+1] = arr[i][j];
+            }
+        }
         int q = Integer.parseInt(br.readLine());
         for(int i = 0; i < q; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             char a = st.nextToken().charAt(0);
             int l = Integer.parseInt(st.nextToken());
             int r = Integer.parseInt(st.nextToken());
-            char[] arr = s.substring(l, r+1).toCharArray();
-            int count = 0;
-            for(char c : arr) {
-                if(c == a)
-                    count++;
-            }
-            sb.append(count).append("\n");
+            sb.append(arr[a - 'a'][r+1] - arr[a - 'a'][l]).append("\n");
         }
         System.out.println(sb);
     }
