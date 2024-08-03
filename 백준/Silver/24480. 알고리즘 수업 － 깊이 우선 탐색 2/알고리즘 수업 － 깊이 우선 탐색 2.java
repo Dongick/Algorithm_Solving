802e390d64f6dc5b3098947534bb3188a3a8281e
Dongick <87112589+Dongick@users.noreply.github.com>
2024-08-03 17:15:44 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 872 ms, Memory: 87660 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int n;
+    static List<List<Integer>> list;
+    static int[] result;
+    static int count = 1;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        list = new ArrayList<>();
+        for(int i = 0; i <= n; i++)
+            list.add(new ArrayList<>());
+        result = new int[n+1];
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+            list.get(n1).add(n2);
+            list.get(n2).add(n1);
+        }
+        for(int i = 1; i <= n; i++)
+            Collections.sort(list.get(i), (o1, o2) -> o2 - o1);
+        dfs(r);
+
+        for(int i = 1; i <= n; i++) {
+            sb.append(result[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int num) {
+        result[num] = count;
+        for(int number : list.get(num)) {
+            if(result[number] == 0) {
+                count += 1;
+                dfs(number);
+            }
+        }
+    }
+}
