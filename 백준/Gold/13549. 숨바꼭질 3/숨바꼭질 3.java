9ffef8b2c4df4878fca5e1fc623d29c5c3a17559
Dongick <87112589+Dongick@users.noreply.github.com>
2024-08-10 18:43:25 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 184 ms, Memory: 24052 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -17,27 +17,22 @@ class Node implements Comparable<Node> {
 }
 
 class Main {
-    static int[] arr = new int[100001];
-    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        for(int i = 0; i <arr.length; i++)
-            arr[i] = -1;
 
-        bfs(n, k);
-        sb.append(arr[k]);
+        sb.append(bfs(n, k));
         System.out.println(sb);
     }
 
-    static void bfs(int n, int k) {
+    static int bfs(int n, int k) {
         Queue<Node> queue = new PriorityQueue<>();
         queue.offer(new Node(n, 0));
-        arr[n] = 0;
         boolean[] check = new boolean[100001];
+        int result = 0;
 
         while(!queue.isEmpty()) {
             Node current = queue.poll();
@@ -45,18 +40,21 @@ class Main {
                 continue;
             check[current.idx] = true;
 
-            if(current.idx - 1 >= 0 && (arr[current.idx - 1] == -1 || arr[current.idx - 1] > current.second + 1)) {
-                arr[current.idx - 1] = current.second + 1;
+            if(current.idx == k) {
+                result = current.second;
+                break;
+            }
+
+            if(current.idx - 1 >= 0) {
                 queue.offer(new Node(current.idx - 1, current.second + 1));
             }
-            if(current.idx + 1 <= 100000 && (arr[current.idx + 1] == -1 || arr[current.idx + 1] > current.second + 1)) {
-                arr[current.idx + 1] = current.second + 1;
+            if(current.idx + 1 <= 100000) {
                 queue.offer(new Node(current.idx + 1, current.second + 1));
             }
-            if(current.idx*2 <= 100000 && (arr[current.idx*2] == -1 || arr[current.idx*2] > current.second)) {
-                arr[current.idx*2] = current.second;
+            if(current.idx*2 <= 100000) {
                 queue.offer(new Node(current.idx*2, current.second));
             }
         }
+        return result;
     }
 }
