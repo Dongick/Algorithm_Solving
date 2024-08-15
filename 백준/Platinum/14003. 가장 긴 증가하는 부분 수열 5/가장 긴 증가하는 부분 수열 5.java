3a244b08605a27d794b376575e46dd3d91ab4b90
Dongick <87112589+Dongick@users.noreply.github.com>
2024-08-15 15:18:55 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 740 ms, Memory: 154660 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -4,52 +4,50 @@ import java.io.*;
 class Main {
     static int n;
     static int[] arr;
-    static int[] trace;
-    static List<int[]> list = new ArrayList<>();
+    static int[] indexArr;
+    static List<Integer> list = new ArrayList<>();
     
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         n = Integer.parseInt(br.readLine());
-        arr = new int[n+1];
-        trace = new int[n+1];
+        arr = new int[n];
+        indexArr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1; i <= n; i++)
+        for(int i = 0; i < n; i++)
             arr[i] = Integer.parseInt(st.nextToken());
 
         int idx = 0;
-        int maxIdx = 1;
-        list.add(new int[] {1, arr[1]});
-        for(int i = 2; i <= n; i++) {
-            if(list.get(idx)[1] < arr[i]) {
-                list.add(new int[] {i, arr[i]});
-                trace[i] = list.get(idx)[0];
-                maxIdx = i;
+        list.add(arr[0]);
+        for(int i = 1; i < n; i++) {
+            if(list.get(idx) < arr[i]) {
+                list.add(arr[i]);
                 idx++;
+                indexArr[i] = idx;
             } else {
                 int index = BinarySearch(0, idx, arr[i]);
-                list.set(index, new int[] {i, arr[i]});
-                if(index > 0)
-                    trace[i] = list.get(index-1)[0];
-                if(idx == 0)
-                    maxIdx = i;
+                list.set(index, arr[i]);
+                indexArr[i] = index;
             }
         }
-        sb.append(idx+1).append("\n");
-        Stack<Integer> stack = new Stack<>();
-        while(maxIdx > 0) {
-            stack.push(arr[maxIdx]);
-            maxIdx = trace[maxIdx];
+        sb.append(list.size()).append("\n");
+        int[] result = new int[list.size()];
+        int size = list.size() - 1;
+        for(int i = n-1; i >= 0; i--) {
+            if(indexArr[i] == size) {
+                result[size] = arr[i];
+                size--;
+            }
         }
-        while(!stack.isEmpty())
-            sb.append(stack.pop()).append(" ");
+        for(int num : result)
+            sb.append(num).append(" ");
         System.out.println(sb);
     }
 
     static int BinarySearch(int start, int end, int num) {
         while(start < end) {
             int mid = (start + end) / 2;
-            if(list.get(mid)[1] < num)
+            if(list.get(mid) < num)
                 start = mid + 1;
             else
                 end = mid;
