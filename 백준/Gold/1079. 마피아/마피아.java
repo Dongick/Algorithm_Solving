269f161ef6bf6fe78eb78c372fa3db9385c1109f
Dongick <87112589+Dongick@users.noreply.github.com>
2024-08-25 16:19:07 +0900

    [Gold II] Title: 마피아, Time: 512 ms, Memory: 21016 KB -BaekjoonHub

---
+++ 백준/Gold/1079. 마피아/마피아.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static int[][] R;
+    static int n;
+    static int result = 0;
+    static int num;
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        R = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++)
+                R[i][j] = Integer.parseInt(st.nextToken());
+        }
+        num = Integer.parseInt(br.readLine());
+        
+        func(0, n, arr, new boolean[n]);
+
+        sb.append(result);
+        
+        System.out.println(sb);
+    }
+
+    static void func(int count, int number, int[] arr, boolean[] check) {
+        if(check[num] || number == 1) {
+            result = Math.max(result, count);
+            return;
+        }
+        
+        if(number % 2 == 1) {
+            int max = 0;
+            int idx = 0;
+            for(int i = 0; i < n; i++) {
+                if(!check[i] && max < arr[i]) {
+                    max = arr[i];
+                    idx = i;
+                }
+            }
+            check[idx] = true;
+            func(count, number-1, arr, check);
+            check[idx] = false;
+        } else {
+            for(int i = 0; i < n; i++) {
+                if(!check[i] && num != i) {
+                    check[i] = true;
+                    for(int j = 0; j < n; j++) {
+                        arr[j] += R[i][j];
+                    }
+                    func(count+1, number-1, arr, check);
+                    check[i] = false;
+                    for(int j = 0; j < n; j++) {
+                        arr[j] -= R[i][j];
+                    }
+                }
+            }
+        }
+    }
+}
