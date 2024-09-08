633bed0a3627ba954ae87a9a53952960d8e94604
Dongick <87112589+Dongick@users.noreply.github.com>
2024-09-09 02:13:47 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 11.38 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,27 +1,12 @@
 class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-        
-        long num = 0;
-        int i = 0;
-        while(n != 0) {
-            num += (n % k) * Math.pow(10, i);
-            n /= k;
-            i++;
-        }
-        
-        while(num != 0) {
-            long number = 0;
-            i = 0;
-            while(num % 10 != 0) {
-                number += (num % 10) * Math.pow(10, i);
-                num = num / 10;
-                i++;
-            }
-            num /= 10;
-            if(number != 0 && func(number)) {
+        String[] arr = Integer.toString(n, k).split("0");
+        for(String s : arr) {
+            if(s.equals(""))
+                continue;
+            if(func(Long.parseLong(s)))
                 answer++;
-            }
         }
         
         return answer;
