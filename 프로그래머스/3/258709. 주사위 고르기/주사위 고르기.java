ca3c9e37875ea90db646c810ec8ad599450092ff
Dongick <87112589+Dongick@users.noreply.github.com>
2024-09-29 23:42:28 +0900

    [level 3] Title: 주사위 고르기, Time: 2555.73 ms, Memory: 408 MB -BaekjoonHub

---
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -0,0 +1,147 @@
+import java.util.*;
+
+class Solution {
+    static int limit;
+    static int n;
+    static int winner = 0;
+    static int[] winnerArr;
+    static Map<Integer, Integer> aMap;
+    static Map<Integer, Integer> bMap;
+    
+    public int[] solution(int[][] dice) {
+        n = dice.length;
+        limit = (int)Math.pow(6, n / 2);
+        winnerArr = new int[n / 2];
+        boolean[] visited = new boolean[n];
+        visited[0] = true;
+        
+        func(0, visited, dice);
+        int[] answer = winnerArr;
+        return answer;
+    }
+    
+    static void func(int count, boolean[] visited, int[][] dice) {
+        if(count == (n - 1) / 2) {
+            aMap = new HashMap<>();
+            bMap = new HashMap<>();
+            
+            for(int i = 0; i < limit; i++) {
+                func2(i, visited, dice);
+            }
+            
+            List<Integer> aKeySet = new ArrayList<>(aMap.keySet());
+            List<Integer> bKeySet = new ArrayList<>(bMap.keySet());
+            
+            Collections.sort(aKeySet);
+            Collections.sort(bKeySet);
+            
+            int aWin = 0;
+            int bWin = 0;
+            int a = 0;
+            int b = 0;
+            int aSum = 0;
+            int bSum = 0;
+            while(a != aKeySet.size() && b != bKeySet.size()) {
+                if(aKeySet.get(a) > bKeySet.get(b)) {
+                    aWin += bMap.get(bKeySet.get(b)) * aMap.get(aKeySet.get(a));
+                    bSum += bMap.get(bKeySet.get(b));
+                    b++;
+                    if(b != bKeySet.size())
+                        bWin += aSum * bMap.get(bKeySet.get(b));
+                    
+                } else if(aKeySet.get(a) < bKeySet.get(b)) {
+                    bWin += aMap.get(aKeySet.get(a)) * bMap.get(bKeySet.get(b));
+                    aSum += aMap.get(aKeySet.get(a));
+                    a++;
+                    if(a != aKeySet.size())
+                        aWin += bSum * aMap.get(aKeySet.get(a));
+                } else {
+                    bSum += bMap.get(bKeySet.get(b));
+                    aSum += aMap.get(aKeySet.get(a));
+                    a++;
+                    b++;
+                    if(a != aKeySet.size())
+                        aWin += bSum * aMap.get(aKeySet.get(a));
+                    if(b != bKeySet.size())
+                        bWin += aSum * bMap.get(bKeySet.get(b));
+                }
+            }
+            
+            if(b == bKeySet.size()) {
+                for(int i = a + 1; i < aKeySet.size(); i++) {
+                    aWin += bSum * aMap.get(aKeySet.get(i));
+                }
+            } else {
+                for(int i = b + 1; i < bKeySet.size(); i++) {
+                    bWin += aSum * bMap.get(bKeySet.get(i));
+                }
+            }
+            
+            if(aWin > bWin) {
+                if(winner < aWin) {
+                    winner = aWin;
+                    int num = 0;
+                    for(int i = 0; i < n; i++) {
+                        if(visited[i]) {
+                            winnerArr[num] = i + 1;
+                            num++;
+                        }
+                    }
+                }
+            } else {
+                if(winner < bWin) {
+                    winner = bWin;
+                    int num = 0;
+                    for(int i = 0; i < n; i++) {
+                        if(!visited[i]) {
+                            winnerArr[num] = i + 1;
+                            num++;
+                        }
+                    }
+                }
+            }
+            
+            return;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                func(count+1, visited, dice);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    static void func2(int num, boolean[] visited, int[][] dice) {
+        int a = 0;
+        int b = 0;
+        int aTemp = num;
+        int bTemp = num;
+        int i = 0;
+        while(i != n) {
+            if(visited[i]) {
+                a += dice[i][aTemp % 6];
+                aTemp /= 6;
+            } else {
+                b += dice[i][bTemp % 6];
+                bTemp /= 6;
+            }
+            i++;
+        }
+        
+        if(aMap.containsKey(a)) {
+            int count = aMap.get(a);
+            aMap.replace(a, count + 1);
+        } else {
+            aMap.put(a, 1);
+        }
+        
+        if(bMap.containsKey(b)) {
+            int count = bMap.get(b);
+            bMap.replace(b, count + 1);
+        } else {
+            bMap.put(b, 1);
+        }
+    }
+}
