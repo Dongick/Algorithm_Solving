dba728a447c01e91468850bd0d41d8355981e045
Dongick <87112589+Dongick@users.noreply.github.com>
2024-10-08 19:59:02 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 10.67 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
@@ -1,6 +1,5 @@
 class Solution {
-    static boolean[][] red;
-    static boolean[][] blue;
+    static boolean[][][] visitColor;
     static int n, m;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
@@ -9,8 +8,7 @@ class Solution {
     public int solution(int[][] maze) {
         n = maze.length;
         m = maze[0].length;
-        red = new boolean[n][m];
-        blue = new boolean[n][m];
+        visitColor = new boolean[2][n][m];
         int[] redStart = new int[2];
         int[] blueStart = new int[2];
         for(int i = 0; i < n; i++) {
@@ -25,8 +23,8 @@ class Solution {
                 }
             }
         }
-        red[redStart[0]][redStart[1]] = true;
-        blue[blueStart[0]][blueStart[1]] = true;
+        visitColor[0][redStart[0]][redStart[1]] = true;
+        visitColor[1][blueStart[0]][blueStart[1]] = true;
         
         func(redStart, blueStart, 0, maze);
         int answer = (result == Integer.MAX_VALUE) ? 0 : result;
@@ -42,25 +40,25 @@ class Solution {
         if(maze[redIdx[0]][redIdx[1]] != 3) {
             for(int i = 0; i < 4; i++) {
                 int[] redTemp = {redIdx[0] + dx[i], redIdx[1] + dy[i]};
-                if(redTemp[0] >= 0 && redTemp[0] < n && redTemp[1] >= 0 && redTemp[1] < m && !red[redTemp[0]][redTemp[1]] && maze[redTemp[0]][redTemp[1]] != 5) {
+                if(checkOutBoundary(redTemp, maze, 0)) {
                     if(maze[blueIdx[0]][blueIdx[1]] != 4) {
                         for(int j = 0; j < 4; j++) {
                             int[] blueTemp = {blueIdx[0] + dx[j], blueIdx[1] + dy[j]};
-                            if(blueTemp[0] >= 0 && blueTemp[0] < n && blueTemp[1] >= 0 && blueTemp[1] < m && !blue[blueTemp[0]][blueTemp[1]] && maze[blueTemp[0]][blueTemp[1]] != 5) {
-                                if(check(redTemp, blueTemp, redIdx, blueIdx)) {
-                                    red[redTemp[0]][redTemp[1]] = true;
-                                    blue[blueTemp[0]][blueTemp[1]] = true;
+                            if(checkOutBoundary(blueTemp, maze, 1)) {
+                                if(checkCross(redTemp, blueTemp, redIdx, blueIdx) && checkSame(redTemp, blueTemp)) {
+                                    visitColor[0][redTemp[0]][redTemp[1]] = true;
+                                    visitColor[1][blueTemp[0]][blueTemp[1]] = true;
                                     func(redTemp, blueTemp, count+1, maze);
-                                    blue[blueTemp[0]][blueTemp[1]] = false;
-                                    red[redTemp[0]][redTemp[1]] = false;
+                                    visitColor[0][redTemp[0]][redTemp[1]] = false;
+                                    visitColor[1][blueTemp[0]][blueTemp[1]] = false;
                                 }
                             }
                         }
                     } else {
-                        if(redTemp[0] != blueIdx[0] || redTemp[1] != blueIdx[1]) {
-                            red[redTemp[0]][redTemp[1]] = true;
+                        if(checkSame(redTemp, blueIdx)) {
+                            visitColor[0][redTemp[0]][redTemp[1]] = true;
                             func(redTemp, blueIdx, count+1, maze);
-                            red[redTemp[0]][redTemp[1]] = false;
+                            visitColor[0][redTemp[0]][redTemp[1]] = false;
                         }
                     }
                 }
@@ -68,22 +66,33 @@ class Solution {
         } else {
             for(int i = 0; i < 4; i++) {
                 int[] blueTemp = {blueIdx[0] + dx[i], blueIdx[1] + dy[i]};
-                if(blueTemp[0] >= 0 && blueTemp[0] < n && blueTemp[1] >= 0 && blueTemp[1] < m && !blue[blueTemp[0]][blueTemp[1]] && maze[blueTemp[0]][blueTemp[1]] != 5) {
-                    if(redIdx[0] != blueTemp[0] || redIdx[1] != blueTemp[1]) {
-                        blue[blueTemp[0]][blueTemp[1]] = true;
+                if(checkOutBoundary(blueTemp, maze, 1)) {
+                    if(checkSame(redIdx, blueTemp)) {
+                        visitColor[1][blueTemp[0]][blueTemp[1]] = true;
                         func(redIdx, blueTemp, count+1, maze);
-                        blue[blueTemp[0]][blueTemp[1]] = false;
+                        visitColor[1][blueTemp[0]][blueTemp[1]] = false;
                     }
                 }
             }
         }
     }
     
-    static boolean check(int[] redTemp, int[] blueTemp, int[] redIdx, int[] blueIdx) {
+    static boolean checkCross(int[] redTemp, int[] blueTemp, int[] redIdx, int[] blueIdx) {
         if(redTemp[0] == blueIdx[0] && redTemp[1] == blueIdx[1] && redIdx[0] == blueTemp[0] && redIdx[1] == blueTemp[1])
             return false;
-        if(redTemp[0] == blueTemp[0] && redTemp[1] == blueTemp[1])
+        return true;
+    }
+    
+    static boolean checkSame(int[] red, int[] blue) {
+        if(red[0] == blue[0] && red[1] == blue[1])
             return false;
         return true;
+    }
+    
+    static boolean checkOutBoundary(int[] temp, int[][] maze, int color) {
+        if(temp[0] >= 0 && temp[0] < n && temp[1] >= 0 && temp[1] < m && !visitColor[color][temp[0]][temp[1]] && maze[temp[0]][temp[1]] != 5) {
+            return true;
+        }
+        return false;
     }
 }
